b6610994a2b78fbedebf6b9751f8728507261a63
20230104Curl <sanaru0104@gmail.com>
2026-08-31 19:59:14 +0900

    Deploy app bundle loader

---
+++ assets/index-DkP0VCHL-loader.js
@@ -0,0 +1,14 @@
+const PARTS = ["index-DkP0VCHL.part00.txt","index-DkP0VCHL.part01.txt","index-DkP0VCHL.part02.txt","index-DkP0VCHL.part03.txt","index-DkP0VCHL.part04.txt","index-DkP0VCHL.part05.txt","index-DkP0VCHL.part06.txt","index-DkP0VCHL.part07.txt","index-DkP0VCHL.part08.txt","index-DkP0VCHL.part09.txt","index-DkP0VCHL.part10.txt","index-DkP0VCHL.part11.txt"];
+const BASE = new URL("./", import.meta.url);
+const chunks = [];
+for (const part of PARTS) {
+  const response = await fetch(new URL(part, BASE));
+  if (!response.ok) throw new Error(`Failed to load ${part}: ${response.status}`);
+  chunks.push(await response.text());
+}
+const bundleUrl = URL.createObjectURL(new Blob([chunks.join("")], { type: "text/javascript" }));
+try {
+  await import(bundleUrl);
+} finally {
+  setTimeout(() => URL.revokeObjectURL(bundleUrl), 0);
+}
