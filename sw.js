2de2e2fe9b151197c6cbfe9eb98a8b3c85ff0e3f
20230104Curl <sanaru0104@gmail.com>
2026-09-02 14:48:19 +0900

    Refresh cache after removing daily scene label

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kanji-kakijun-v15";
+const CACHE = "kanji-kakijun-v16";
 const ROOT = new URL("./", self.registration.scope).toString();
 const assetUrl = (path) => new URL(path, ROOT).toString();
 const KANJI = [
